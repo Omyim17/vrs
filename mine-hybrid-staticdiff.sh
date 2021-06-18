12e54f1bd043136ee4b61dfa16066b67d845f9ee
Omyim17 <86036284+Omyim17@users.noreply.github.com>
2021-06-18 21:54:01 +0700

    Update mine-hybrid-staticdiff.sh

--- mine-hybrid-staticdiff.sh
+++ mine-hybrid-staticdiff.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDWx2Sa8iyeLVTMUUi5ypY357ZXH5tNf56.hellminer -p d=16384s,hybrid --cpu 2
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWiK7SBz2gyjywBgAKCAyLS3XrPRTckJvY.Jayper -p d=16384s,hybrid --cpu 2
